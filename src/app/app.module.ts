fe7a3c974ed4e74663d0f5be43e64dd3f1ad5048
H0clar <julioromero1995@gmail.com>
2024-06-17 22:31:50 -0400

    projects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './landing/home/home.component';
 import { AboutComponent } from './landing/about/about.component';
-import routes from './app.routes';
 import { NavbarComponent } from './landing/navbar/navbar.component';
+import { importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { ProjectsComponent } from './landing/projects/projects.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { NavbarComponent } from './landing/navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    ProjectsComponent // Importa el componente independiente
   ],
   providers: [],
   bootstrap: [AppComponent]
